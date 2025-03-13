38e76dd777d3485fad70223f5cbe6aeceaf92079
Nikhita Sathish <nikhita.sathish01@gmail.com>
2025-03-13 14:40:56 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,7 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS360\n";
+  cout << "Nikhita Sathish\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
